8ea648a38b650f84f4f450b72a644e8862e12165
Satya Prakash <satyaprakash6275@gmail.com>
2022-04-18 16:13:28 +0530

    connection debuged

--- config/database.ts
+++ config/database.ts
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 dotenv.config();
 
 // const { MONGO_URI } = process.env;
+const database = process.env.USERNAME;
+const password = process.env.PASSWORD;
 
 exports.connect = async () => {
   // Connecting to the database
   try {
-    await mongoose.connect('mongodb+srv://satyaprakash:mongodb@satya.lberk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+    await mongoose.connect(`mongodb+srv://${database}:${password}@satya.lberk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
   } catch (error) {
     console.log(error);
   }
